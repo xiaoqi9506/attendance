11ab7c6c86abe2e08613bb141ab666efd9b7ef67
xiaoqi <m15218408986@163.com>
2019-03-20 20:11:35 +0800

    提代码

--- attendance-provider/src/main/java/cn/howtoplay/attendance/mapper/LeaveInfoMapper.java
+++ attendance-provider/src/main/java/cn/howtoplay/attendance/mapper/LeaveInfoMapper.java
@@ -1,9 +1,10 @@
 package cn.howtoplay.attendance.mapper;
 
+import cn.howtoplay.attendance.domain.eo.LeaveInfo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
  * @author xiaoqi on 2019/3/19
  */
-public interface LeaveInfoMapper extends BaseMapper {
+public interface LeaveInfoMapper extends BaseMapper<LeaveInfo> {
 }
